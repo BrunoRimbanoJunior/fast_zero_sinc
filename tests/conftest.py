f5960f53d7b720afab34ddb242c54350eb36285f
Bruno Rimbano Junior <brunorimbanojunior@MacBook-Pro-de-Bruno.local>
2024-07-23 12:09:13 -0300

    Create database with migritions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,33 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from fast_zero.app import app
+from fast_zero.model import table_registry
 
+
+"""O arquivo conftest ou arquivo de configuracao do teste,
+deve conter trechos de codigos que serão utilizados varias
+vezes durante a criação de testes
+
+Neste cado temos o client da Api e a Conexão com o banco de dados
+
+Principio do DRY, não repetir codigo.
+"""
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+# para que os testes sejam deterministicos cria-se um banco em memoria
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
---
+++ tests/test_db.py
@@ -0,0 +1,16 @@
+from sqlalchemy import select
+
+from fast_zero.model import User
+
+
+def test_create_user(session):
+    user = User(username='Teste', email='teste@email.com', password='teste123')
+    session.add(user)
+    session.commit()
+    # session.refresh(user)
+    result = session.scalar(
+        select(User).where(User.email == 'teste@email.com')
+    )
+
+    assert result.id == 1
+    assert result.username == 'Teste'
